8ec9b0065b7c248d512257d49824a658ea2779e8
sargis petrosyan <saqomax@gmail.com>
2025-02-12 16:31:14 +0100

    Polimorphism

---
+++ Polymorphism.py
@@ -0,0 +1,33 @@
+from abc import ABC
+
+'''Polimorphism is using same function for handle differnet purpose'''
+class Vehicle(ABC):
+    @classmethod
+    def drive(self, car_model: str) -> None: ...
+
+
+class BMW(Vehicle):
+    def drive(self, car_model: str) -> None:
+        print(f"BMW model:{car_model} is driving!!!")
+
+
+class Mercedes(Vehicle):
+    def drive(self, car_model: str) -> None:
+        print(f"Mercedes model:{car_model} is driving!!!")
+
+
+class Volvo(Vehicle):
+    def drive(self, car_model: str) -> None:
+        print(f"Volvo model{car_model} is driving!!!")
+
+
+def buy_car(car_model: Vehicle, model: str):
+    car_model.drive(model)
+
+
+bmw: BMW = BMW()
+mercedes: Mercedes = Mercedes()
+volvo: Volvo = Volvo()
+
+buy_car(car_model=bmw, model="m3")
+buy_car(car_model=mercedes, model="s200")
