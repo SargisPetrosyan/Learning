220a5458290871f46ce11e81f4d3b9a6010c439d
sargis petrosyan <saqomax@gmail.com>
2025-02-17 15:15:38 +0100

    Logging

---
+++ Logging/calculation.py
@@ -0,0 +1,66 @@
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+    )
+
+file_handler = logging.FileHandler('calculator.log')
+file_handler.setLevel(logging.ERROR)
+
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+
+def add(x: int | float, y: int | float):
+    return x + y
+
+def subtract(x: int | float, y: int | float):
+    return x - y
+
+
+def multiply(x: int | float, y: int | float):
+    return x * y
+
+def divide(x: int | float, y: int | float):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        logger.error('tyred by divide by 0')
+    else:
+        return result
+    
+
+
+
+x = 30
+y = 0
+
+add_result = add(x=x, y=y)
+logger.debug(f"add_result: {add_result}")
+
+subtract_result = subtract(x=x, y=y)
+logger.debug(f"subtract_result: {subtract_result}")
+
+multiply_result = multiply(x=x, y=y)
+logger.debug(f"multiply_result: {multiply_result}")
+
+divide_result = divide(x=x, y=y )
+logger.debug(f"divide_result: {divide_result}")
+
+
+# logging types
+# 
+# debug()
+# info()
+# warning()
+# error()
+# critical()
---
+++ Logging/create_user.py
@@ -0,0 +1,27 @@
+import logging
+import calculation
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+    )
+
+file_handler = logging.FileHandler('user.log')
+
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+class User:
+    
+    def __init__(self, name: str, lastname: str, age: int) -> None:
+        self.username = name
+        self.user_lastname = lastname
+        self.user_age = age
+        logger.info(f"user {self.username} was created")
+
+user_1 = User(name='Saqo', lastname='Petrosyan', age=29)
+        
